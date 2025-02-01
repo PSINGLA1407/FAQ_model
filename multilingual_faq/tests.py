2e9e0172dc559fdd4dcbe02679d5fa4030bbd4bd
Pranav Singla <PRANAVSINGLA1407@GMAIL.COM>
2025-02-02 02:06:01 +0530

    answer translation updated

--- multilingual_faq/tests.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from myapp.models import MyModel
-
-@pytest.mark.django_db
-def test_my_model_creation():
-    instance = MyModel.objects.create(field1='value1', field2='value2')
-    assert instance.pk is not None
